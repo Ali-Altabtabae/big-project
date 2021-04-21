c90ec0d4992b6277c2a939a54556e57bb6c7d5c2
AliMJ2021 <bo7ussain12@gmail.com>
2021-04-21 19:53:09 +0300

    myteam page

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -7,18 +7,18 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Signin from "../authentication/Signin";
 import Home from "../Home";
 import About from "../About";
+import MyTeam from "../MyTeam";
 
 const { Navigator, Screen } = createStackNavigator();
 
 const RootNavigator = () => {
   return (
     <Navigator initialRouteName="Signin">
-      <Screen
-        name="Signin"
-        component={Signin}
-        options={{ headerShown: false }}
-      />
+
+      <Screen name="MyTeam" component={MyTeam} />
       <Screen name="Home" component={Home} />
+
+    
       <Screen name="About" component={About} />
     </Navigator>
   );
