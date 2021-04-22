88a2c05ba48490e1f892a5e1dad366ac32ef42b9
Ali-Altabtabae <78157385+Ali-Altabtabae@users.noreply.github.com>
2021-04-22 03:26:48 +0300

    Styled

--- components/Fields.js
+++ components/Fields.js
@@ -1,7 +1,30 @@
 import React from "react";
 import FieldList from "./field/FieldList";
+import authStore from "../stores/authStore";
+import {
+  HomeContainer,
+  SignOutButtonText,
+  AuthHeader,
+  HeaderTitle,
+  SignOutButton,
+} from "../styles";
 
-const Fields = () => {
-  return <FieldList />;
+const Fields = ({ navigation }) => {
+  const handleSubmit = async () => {
+    authStore.signout();
+    navigation.replace("Signin");
+  };
+
+  return (
+    <>
+      <AuthHeader>
+        <HeaderTitle onPress={() => navigation.replace("Home")}>Home</HeaderTitle>
+        <SignOutButton onPress={handleSubmit}>
+          <SignOutButtonText>Sign out</SignOutButtonText>
+        </SignOutButton>
+      </AuthHeader>
+        <FieldList />
+    </>
+  );
 };
 export default Fields;
--- styles.js
+++ styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components/native";
 
 export const TopStyling = styled.View`
-  height: 40%;
+  height: 20%;
   align-items: center;
   justify-content: center;
   background-color: rgba(100, 40, 60, 0.4);
@@ -19,7 +19,7 @@ export const OverLayContainer = styled.View`
 `;
 
 export const BottomStyling = styled.View`
-  height: 40%;
+  height: 60%;
   align-items: center;
   justify-content: center;
   background-color: rgba(0, 99, 71, 0.4);
@@ -31,18 +31,34 @@ export const ButtonStyled = styled.Text`
 `;
 
 export const AuthContainer = styled.View`
-  flex: 1;
+  flex: 8;
   align-self: stretch;
   justify-content: center;
   align-items: center;
-  background-color: #fff;
+  background-color: #05668d;
   padding-right: 60px;
   padding-left: 60px;
+`;
+
+export const AuthHeader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #05668d;
+  border-bottom-width: 1px;
+  border-color: #f5e4d7;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-size: 16px;
+  margin-top: 20px;
+  color: #f5e4d7;
 `;
 
 export const AuthTitle = styled.Text`
   font-size: 24px;
   margin-bottom: 20px;
+  color: #f5e4d7;
 `;
 
 export const AuthTextInput = styled.TextInput`
@@ -51,23 +67,66 @@ export const AuthTextInput = styled.TextInput`
   height: 40px;
   margin-bottom: 30px;
   border-bottom-width: 1px;
+  border-color: #F5E4D7
+  color: #F5E4D7
 `;
 
 export const AuthButton = styled.TouchableOpacity`
   align-self: stretch;
   align-items: center;
   padding: 20px;
-  background-color: #fff;
+  background-color: #f5e4d7;
   margin-top: 30px;
+`;
+
+export const SignOutButton = styled.TouchableOpacity`
+  align-self: stretch;
+  position: absolute;
+  padding-top: 20px;
+  margin-right: 15px;
+  right: 1;
+`;
+
+export const HomeContainer = styled.View`
+  flex: 8;
+  align-self: stretch;
+  align-items: center;
+  background-color: #05668d;
+  padding-right: 60px;
+  padding-left: 60px;
+`;
+
+export const HomeButton = styled.TouchableOpacity`
+  align-self: stretch;
+  align-items: center;
+  padding: 20px;
+  background-color: #05668d;
+  margin-top: 30px;
+  border: 3px;
+  border-color: #f5e4d7;
+  border-radius: 5px; 
+`;
+
+export const HomeButtonText = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+  color: #f5e4d7;
+`;
+
+export const SignOutButtonText = styled.Text`
+  font-size: 18px;
+  color: #f5e4d7;
 `;
 
 export const AuthButtonText = styled.Text`
   font-weight: bold;
   font-size: 18px;
+  color: #05668d;
 `;
 
 export const AuthOther = styled.Text`
   margin-top: 15px;
+  color: #e5d1d0;
 `;
 
 export const TeamItemStyled = styled.Text`
